7ca04f3553508c6ca71b8f0b3102b033e9878d59
Paula <paula.m.villarruel@gmail.com>
2023-03-04 20:43:08 -0300

    mi primer commit

---
+++ js/curriculum.js
@@ -0,0 +1,11 @@
+function mensaje(){   
+   document.getElementById("button").innerHTML="Disponibilidad horaria y con muchas ganas de trabajar";
+};
+
+function capturar(){
+                
+   var nombreCaptura = document.getElementById("nombre").value;
+   var apellidoCaptura = document.getElementById("apellido").value;
+        
+   document.write( nombreCaptura + apellidoCaptura+' Muchas gracias por visitar esta página, espero pronta respuesta.'); 
+};
